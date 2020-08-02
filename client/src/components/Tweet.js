cc96628644ada811698b32e0fb010d473d8c1699
“Marc <marc_piantone@live.fr>
2020-08-02 16:21:28 -0400

    Fixing HomeFeed

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -1,34 +1,80 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
 import { format } from "date-fns";
 
 import styled, { keyframes } from "styled-components";
 import { COLORS } from "./constants";
 
 const Tweet = ({ tweetId, tweet }) => {
-  console.log(tweet);
-
-  //const shortDate = format(tweet.timestamp.split("T")[0], "MMM-d");
-  var d = Date.parse(tweet.timestamp.split("T")[0]);
-  console.log(d);
+  console.log();
+  const tweetedOn = format(new Date(tweet.timestamp.split("T")[0]), "MMM do");
+  console.log(
+    tweet.timestamp.split("T")[0] +
+      " does not compare to " +
+      tweetedOn +
+      " WHY, SCOTT ?"
+  );
   return (
     <LI>
-      <Name to="/">{tweet.author.displayName}</Name>
-      <Handle>@{tweet.author.handle}</Handle>
-      <TweetedOn>{tweet.timestamp}</TweetedOn>
+      <Content>
+        <Name to="/">{tweet.author.displayName}</Name>
+        <TweetInfos>
+          <Handle>@{tweet.author.handle} </Handle>
+          <TweetedOn>· {tweetedOn}</TweetedOn>
+        </TweetInfos>
+        <Status>{tweet.status}</Status>
+        {tweet.media.map((media) => {
+          if (media.type === "img") {
+            return <ImgMedia src={media.url} alt="brokenLink" />;
+          } else {
+            return <span>Media unrecognized</span>;
+          }
+        })}
+        <Divider />
+      </Content>
     </LI>
   );
 };
 
+const LI = styled.li`
+  border-left: solid 1px ${COLORS.greyish};
+  border-right: solid 1px ${COLORS.greyish};
+`;
+
+const Content = styled.div`
+  width: 700px;
+  padding: 15px 15px 0px 15px;
+`;
+
 const Name = styled(NavLink)`
   font-weight: bolder;
+  color: black;
+`;
+
+const TweetInfos = styled.span`
+  color: ${COLORS.grey};
 `;
 
 const Handle = styled.span``;
 
 const TweetedOn = styled.span``;
 
-const LI = styled.li``;
+const Status = styled.p`
+  margin-bottom: 8px;
+`;
+
+const Media = styled.div``;
+
+const ImgMedia = styled.img`
+  margin-bottom: 8px;
+  border-radius: 20px;
+  max-width: 670px;
+`;
+
+const Divider = styled.div`
+  top: 10px;
+  height: 1px;
+  background: ${COLORS.greyish};
+`;
 
 export default Tweet;
--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -3,5 +3,6 @@ export const COLORS = {
   primary: "hsl(258deg, 100%, 50%)",
   hover: "hsl(258deg, 100%, 95%)",
   greyish: "hsl(0deg, 0%, 80%)",
+  grey: "hsl(0deg, 0%, 50%)",
   // Add more colors as needed!
 };
